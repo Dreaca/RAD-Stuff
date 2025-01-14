3b7ddaf2add788b04b43220d1588bff66cacaf3a
buddhika pathum <78160513+Dreaca@users.noreply.github.com>
2025-01-14 21:24:16 +0530

    Create Demo2.ts

---
+++ Node/Demo2.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from 'path'
+
+const app = express()
+
+const dash: string = path.join(process.cwd(), 'Dashboard.html');
+const adc: string = path.join(process.cwd(), 'AddCustomer.html');
+
+app.use('/add',(req,res,next)=>{
+    console.log(" Add middleware")
+    res.send('')
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("Middleware 2")
+})
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000")
+})
